967acb50a937f67930105ee48c7e66f80173f154
Matthew <mcraig150@gmail.com>
2020-05-24 02:45:06 -0400

    Changed HTML layout and added local storage to save events

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 
-var check;
-
 function start(){
   $('#currentDay').append(moment().format('MMM Do, YYYY'));
 
@@ -10,80 +8,45 @@ function start(){
 
 function checkTime(){
 
-    var time = moment().format("LT");
+    var hour = moment().hours();
 
-    var mTime = moment(time, ["h:mm"]).format("HH:mm");
+    for(var i = 9; i <= 17; i++) {
 
-    time = mTime;
+        var target = "#" + i;
+        var block = parseInt($(target).attr("id"));
 
-    
-
-    var tNum = parseInt(time.substring(0,time.indexOf(":")));
-
-    console.log(tNum);
-
-    if (tNum > 9) {
-        $("#9").css("background-color", "#e09792");
-    }
-    else if (tNum === 9) {
-        $("#9").css("background-color", "#9dcf9b");
-    }
-
-    if (tNum > 10) {
-        $("#10").css("background-color", "#e09792");
-    }
-    else if (tNum === 10) {
-        $("#10").css("background-color", "#9dcf9b");
+        if(block > hour) {
+            $(target).css("background-color", "#91cf9c")
+        }
+        else if(block < hour) {
+            $(target).css("background-color", "#ed5a61")
+        }
+        else if(block === hour) {
+            $(target).css("background-color", "#bab9b6")
+        }
     }
 
-    if (tNum > 11) {
-        $("#11").css("background-color", "#e09792");
-    }
-    else if (tNum === 11) {
-        $("#11").css("background-color", "#9dcf9b");
-    }
 
-    if (tNum > 12) {
-        $("#12").css("background-color", "#e09792");
-    }
-    else if (tNum === 12) {
-        $("#12").css("background-color", "#9dcf9b");
-    }
+    saveData();
 
-    if (tNum > 13) {
-        $("#13").css("background-color", "#e09792");
-    }
-    else if (tNum === 13) {
-        $("#13").css("background-color", "#9dcf9b");
-    }
 
-    if (tNum > 14) {
-        $("#14").css("background-color", "#e09792");
-    }
-    else if (tNum === 14) {
-        $("#14").css("background-color", "#9dcf9b");
-    }
+}
 
-    if (tNum > 15) {
-        $("#15").css("background-color", "#e09792");
-    }
-    else if (tNum === 15) {
-        $("#15").css("background-color", "#9dcf9b");
-    }
+function saveData(){
 
-    if (tNum > 16) {
-        $("#16").css("background-color", "#e09792");
-    }
-    else if (tNum === 16) {
-        $("#16").css("background-color", "#9dcf9b");
-    }
-    if (tNum > 17) {
-        $("#17").css("background-color", "#e09792");
-    }
-    else if (tNum === 17) {
-        $("#17").css("background-color", "#9dcf9b");
-    }
+    $(".saveBtn").click(function(){
+        var save = parseInt($(this).attr("id"));
+        var event = $("#" + save + "t").val();
+        console.log(event);
+        localStorage.setItem("" + save, event);
+        console.log(save);
+    });
 
+    for(var i = 9; i <= 17; i++) {
+        var text = "#" + i + "t";
+        $(text).val(localStorage.getItem("" + i));
+        console.log(localStorage.getItem("" + i));
+    }
 
 }
 
